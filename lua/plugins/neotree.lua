546e9f3b3d74d950b5d69b77b967c71c69bfce61
Adnan Joraid <adnanjoraidca@gmail.com>
2025-07-06 01:31:52 -0400

    add neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+    cmd = "Neotree",
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>e", ":Neotree focus <CR>", {})
+	end,
+}
