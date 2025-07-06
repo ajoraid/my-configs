2c570b1b82179435c8e5dcda621819ad4fb59504
Adnan Joraid <adnanjoraidca@gmail.com>
2025-07-06 00:20:54 -0400

    remove ~ character from empty line

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set expandtab")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.opt.fillchars = { eob = " " }
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
