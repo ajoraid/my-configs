500b8d01d19764db9d547b80eb2c04584a1229ae
Adnan Joraid <adnanjoraidca@gmail.com>
2025-07-06 20:19:38 -0400

    add lspconfig, configure code action and other stuff + ui

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "ols" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+            vim.keymap.set('n', '<leader>fo', function()
+                vim.lsp.buf.format { async = true }
+            end, opts)
+        end
+    }
+}
