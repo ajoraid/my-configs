98ba87aed17c292e2cb2026a6b0001b9f8ae5e3a
Adnan Joraid <adnanjoraidca@gmail.com>
2025-07-12 13:59:05 -0400

    modify vim :make command to run odin files/packages

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -3,5 +3,21 @@ vim.cmd("set tabstop=4")
 vim.cmd("set expandtab")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.api.nvim_create_user_command("Make", function(opts)
+  local cmd = "odin run"
+  local args = vim.split(opts.args, " ")
+
+  if #args == 1 and args[1] == "-f" then
+    local file = vim.fn.expand("%:p")
+    cmd = cmd .. " " .. vim.fn.shellescape(file) .. " -file"
+  elseif #args == 2 and args[1] == "-p" then
+    cmd = cmd .. " " .. vim.fn.shellescape(args[2])
+  else
+    cmd = cmd .. " ."
+  end
+
+  vim.cmd("make! " .. cmd)
+end, { nargs = "*" })
+vim.opt.makeprg = ""
 vim.opt.fillchars = { eob = " " }
 vim.g.mapleader = " "
