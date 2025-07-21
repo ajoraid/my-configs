10cfaefb738c0aad096a44eea381322b6ac0049f
Adnan Joraid <adnanjoraidca@gmail.com>
2025-07-20 21:35:33 -0400

    add back lspconfig errors ui and add completions using cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,17 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = { "lua_ls", "ols" }
@@ -15,11 +20,27 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.solargraph.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+
             lspconfig.lua_ls.setup({})
             vim.diagnostic.config({
-                virtual_text = false,
+                virtual_text = true,
                 signs = false,
                 underline = false
             })
