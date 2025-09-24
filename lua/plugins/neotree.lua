5176ced9b8c0fdf1cc19a413dc4a2137b9a3ec11
Adnan Joraid <adnanjoraidca@gmail.com>
2025-09-24 00:56:44 -0400

    replace : with cmd to disable command echo

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -23,8 +23,8 @@ return {
             },
         })
 
-        vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-        vim.keymap.set("n", "<leader>e", ":Neotree focus <CR>", {})
+        vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", {})
+        vim.keymap.set("n", "<leader>bf", "<cmd>Neotree buffers reveal float<CR>", {})
+        vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus <CR>", {})
     end,
 }
