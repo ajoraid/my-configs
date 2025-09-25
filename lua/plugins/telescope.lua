e76723855657922b464eb0c1ef23c25d249aadfa
Adnan Joraid <adnanjoraidca@gmail.com>
2025-09-25 19:22:29 -0400

    update file_ignore_patterns to hide binaries, images, archives, and .git/.DS_Store files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,35 @@ return {
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                defaults = {
+                    file_ignore_patterns = {
+                        "%.git/",
+                        "node_modules",
+                        "vendor/",
+
+                        "%.DS_Store",
+                        "%.AppleDouble",
+                        "%.LSOverride",
+                        "__MACOSX/",
+
+                        "build/",
+                        "dist/",
+                        "%.o$", "%.a$", "%.out$", "%.dylib$", "%.so$",
+                        "%.exe$", "%.dll$",
+                        "%.class$", "%.jar$", "%.war$",
+                        "%.pyc$", "%.pyo$", "__pycache__",
+
+                        "%.zip$", "%.tar$", "%.tar.gz$", "%.7z$", "%.rar$",
+
+                        "%.jpg$", "%.jpeg$", "%.png$", "%.gif$", "%.bmp$", "%.svg$", "%.ico$",
+                        "%.mp3$", "%.mp4$", "%.mkv$", "%.avi$", "%.wav$", "%.flac$",
+
+                        "^[^.]+$",
+                    }
+                }
+            })
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -12,15 +41,14 @@ return {
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
-        require("telescope").setup {
-            extensions = {
-                ["ui-select"] = {
-                    require("telescope.themes").get_dropdown {}
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    }
                 }
             }
-        }
             require("telescope").load_extension("ui-select")
         end
     },
 }
-
