0b9df903ce15e50e829370a6a26a64d882f33824
Adnan Joraid <adnanjoraidca@gmail.com>
2026-02-02 23:11:40 -0500

    remove italic from theme

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,5 +2,11 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
         vim.cmd("colorscheme kanagawa-dragon")
+        vim.cmd([[
+            highlight Comment gui=NONE
+            highlight Keyword gui=NONE
+            highlight Function gui=NONE
+            highlight Type gui=NONE
+        ]])
     end
 }
