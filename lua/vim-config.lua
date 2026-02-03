9656ab682fa675d4e65d564e3b63ed2e845e8a3d
Adnan Joraid <adnanjoraidca@gmail.com>
2026-02-02 23:19:36 -0500

    hide redundant mode text and clear ui

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -4,19 +4,19 @@ vim.cmd("set expandtab")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.api.nvim_create_user_command("Odin", function(opts)
-  local cmd = "odin run"
-  local args = vim.split(opts.args, " ")
+    local cmd = "odin run"
+    local args = vim.split(opts.args, " ")
 
-  if #args == 1 and args[1] == "-f" then
-    local file = vim.fn.expand("%:p")
-    cmd = cmd .. " " .. vim.fn.shellescape(file) .. " -file"
-  elseif #args == 2 and args[1] == "-p" then
-    cmd = cmd .. " " .. vim.fn.shellescape(args[2])
-  else
-    cmd = cmd .. " ."
-  end
+    if #args == 1 and args[1] == "-f" then
+        local file = vim.fn.expand("%:p")
+        cmd = cmd .. " " .. vim.fn.shellescape(file) .. " -file"
+    elseif #args == 2 and args[1] == "-p" then
+        cmd = cmd .. " " .. vim.fn.shellescape(args[2])
+    else
+        cmd = cmd .. " ."
+    end
 
-  vim.cmd("make! " .. cmd)
+    vim.cmd("make! " .. cmd)
 end, { nargs = "*" })
 vim.opt.makeprg = ""
 vim.opt.fillchars = { eob = " " }
@@ -24,3 +24,6 @@ vim.opt.textwidth = 80
 -- considering the below later..
 -- vim.opt.colorcolumn = "80"
 vim.g.mapleader = " "
+vim.opt.laststatus = 3
+vim.opt.showmode = false
+vim.opt.cmdheight = 0
