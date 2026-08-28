0e4715ecc58779d4c26545085daeeeaf099858f6
Adnan Joraid <adnanjoraidca@gmail.com>
2026-08-27 20:30:23 -0700

    configure swift LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,8 +38,9 @@ return {
             lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
-
-            lspconfig.lua_ls.setup({})
+            lspconfig.sourcekit.setup({
+                capabilities = capabilities
+            })
             vim.diagnostic.config({
                 virtual_text = true,
                 signs = false,
